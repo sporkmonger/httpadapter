794907db4a72cd1cdc719404afeabbd79eb972a3
Bob Aman <bobaman@google.com>
2011-01-05 00:39:09 +0000

    Incrementing version number and updating CHANGELOG.
    git-svn-id: https://ruby-httpadapter.googlecode.com/svn/trunk@5 edbb5dda-4979-f80a-53c2-c30fb573866f

--- lib/httpadapter/version.rb
+++ lib/httpadapter/version.rb
@@ -18,7 +18,7 @@ unless defined? HTTPAdapter::VERSION
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 2
-      TINY  = 0
+      TINY  = 1
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
