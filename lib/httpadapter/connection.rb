61eebfbca1cecddb36ffeae6161a3e03fdecf150
Bob Aman <bobaman@google.com>
2011-01-14 23:21:51 +0000

    Removed circular require.
    git-svn-id: https://ruby-httpadapter.googlecode.com/svn/trunk@9 edbb5dda-4979-f80a-53c2-c30fb573866f

--- lib/httpadapter/connection.rb
+++ lib/httpadapter/connection.rb
@@ -12,8 +12,6 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-require 'httpadapter'
-
 module HTTPAdapter #:nodoc:
   class Connection
     def initialize(host, port, connection, options={})
