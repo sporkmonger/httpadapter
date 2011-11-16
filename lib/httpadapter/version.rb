3337f9261b3f25ffd82db63489c9b076cee0fb42
Bob Aman <bobaman@google.com>
2011-11-16 16:45:15 +0300

    Updated CHANGELOG and version.

--- lib/httpadapter/version.rb
+++ lib/httpadapter/version.rb
@@ -19,7 +19,7 @@ unless defined? HTTPAdapter::VERSION
     module VERSION
       MAJOR = 1
       MINOR = 0
-      TINY  = 0
+      TINY  = 1
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
